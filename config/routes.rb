e58bc760fefd459a3b88f078ce8efa332cd3e8bc
sachin-sivadasan <sachinkarunagappally@gmail.com>
2021-06-09 17:03:43 +0530

    graphql

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   mount Blorgh::Engine => "/blorgh"
   mount Hengine::Engine => "/hengine"
   mount Qengine::Engine => "/qengine"
